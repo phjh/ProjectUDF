476ac42f373c1f0bd435ccf8e5909a09468db8b9
gusdnr1231 <1231phw@gmail.com>
2024-03-07 11:46:05 +0900

    24_03_07
    Time Manager 스크립트 작성
    
    - 게임의 전체적인 시간 흐름을 관리해줄 시간 매니저를 만들었습니다.
    - 현재는 내부에 타이머 기능만을 추가하였습니다.

---
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Core/TimeManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.BoolParameter;
+
+public class TimeManager : MonoSingleton<TimeManager>
+{
+    public float NowTime { get; private set; }
+    public float MaxTime { get; private set; }
+
+    private bool isWorkingTimer;
+    public bool IsWorkingTimer => isWorkingTimer;
+
+    public void ResetTimer()
+    {
+        NowTime = MaxTime;
+    }
+
+    public void StopTimer(bool isReset = false, bool isStop = true) //타이머 정지 (리셋할 것인가? 정지하는 것인가?)
+    {
+        isWorkingTimer = !isStop;
+        if(isReset) { ResetTimer(); }
+    }
+
+    public void StartTimer()
+    {
+        isWorkingTimer = true;
+        WorkingTimer();
+    }
+
+    private IEnumerator WorkingTimer()
+    {
+        while(NowTime > 0 && isWorkingTimer)
+        {
+            if(IsWorkingTimer == false) yield break;
+			NowTime -= Time.deltaTime;
+			DisplayTime(NowTime);
+
+            if(NowTime <= 0)
+            {
+                StopTimer();
+            }
+        }
+		StopTimer(true);
+		yield return null;
+    }
+
+	private void DisplayTime(float timeToDisplay) //보여줄 시간 텍스트화
+	{
+		var t0 = (int)timeToDisplay; //전체 시간 인트로 받기
+		var m = t0 / 60; //분 단위 값
+		var s = (t0 - m * 60); //초 단위 값
+		var ms = (int)((timeToDisplay - t0) * 100); //밀리세컨드 앞 2자리
+
+		//TimeText.text = $"{m:00}:{s:00}:{ms:00}";
+	}
+}
