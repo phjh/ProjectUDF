a869bf13e0827b376afd7475aea7cb653abdf753
gusdnr1231 <1231phw@gmail.com>
2024-03-15 15:20:43 +0900

    24_03_15
    Stat 스크립트에 설명용 간이 주석 추가

--- ProjectUDF/Assets/01. Scripts/gusdnr/Core/Stat.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Core/Stat.cs
@@ -19,24 +19,24 @@ public class Stat
 		return finalValue;
 	}
 
-	public void SetValue(float finalValue)
+	public void SetRealValue(float finalValue) //버프가 존재하지 않는 실 적용 값 세팅
 	{
 		realValue = finalValue;
 	}
 
-	public void SetDefaultValue(float value)
+	public void SetDefaultValue(float value) //게임 시작시 가장 초기 값 세팅
 	{
 		baseValue = value;
 		realValue = baseValue;
 	}
 
-	public void AddModifier(float value)
+	public void AddModifier(float value) //Stat 능력치 증가 수치를 Modifiers 리스트에 추가해 후 적용
 	{
 		if (value != 0)
 			modifiers.Add(value);
 	}
 
-	public void RemoveModifier(float value)
+	public void RemoveModifier(float value) //Modifiers 리스트에서 value 값이 존재할 때 삭제하기
 	{
 		if (value != 0)
 			modifiers.Remove(value);
