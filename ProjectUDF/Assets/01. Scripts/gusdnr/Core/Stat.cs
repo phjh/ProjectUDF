6048ac5bee44304baa30908c8310653d9bf94cf7
gusdnr1231 <1231phw@gmail.com>
2024-03-18 19:35:37 +0900

    24_03_18
    폰트 추가
    Stat 스크립트 수정

--- ProjectUDF/Assets/01. Scripts/gusdnr/Core/Stat.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Core/Stat.cs
@@ -6,38 +6,31 @@ using UnityEngine;
 public class Stat
 {
 	[SerializeField] private float baseValue;
-	[HideInInspector] public float realValue;
 	public List<float> fixedModifiers = new List<float>();
 	public List<float> persentedModifiers = new List<float>();
 
-	public void ResetValue() //게임 오버나 초기값으로 재 세팅용 변수
-	{
-		realValue = baseValue;
-	}
-
 	public float GetValue()
 	{
-		float finalValue = realValue;
+		float finalValue = baseValue;
 		for (int i = 0; i < fixedModifiers.Count; ++i)
 		{
 			finalValue += fixedModifiers[i];
 		}
 		for (int i = 0; i < persentedModifiers.Count; ++i)
 		{
-			finalValue *= persentedModifiers[i] * 0.01f;
+			finalValue *= 1 + (persentedModifiers[i] * 0.01f);
 		}
 		return finalValue;
 	}
 
 	public void SetRealValue(float finalValue) //버프가 존재하지 않는 실 적용 값 세팅
 	{
-		realValue = finalValue;
+		baseValue = finalValue;
 	}
 
 	public void SetDefaultValue(float value) //게임 시작시 가장 초기 값 세팅
 	{
 		baseValue = value;
-		realValue = baseValue;
 	}
 
 	public void AddModifier(float value, bool IsPersent = false) //Stat 능력치 증가 수치를 Modifiers 리스트에 추가해 후 적용
