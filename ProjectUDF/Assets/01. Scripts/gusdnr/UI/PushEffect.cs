3bf3d5ca9bf6bd607997e60423ec6a74ee1f582d
gusdnr1231 <1231phw@gmail.com>
2024-03-19 11:03:07 +0900

    24_03_19
    광석 채굴 UI 제작 완료
    숨기는 기능 및 다른 UI/시스템과의 상호작용은 추후 작업 예정

---
+++ ProjectUDF/Assets/01. Scripts/gusdnr/UI/PushEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PushButtonEffect : MonoBehaviour
+{
+	public void OnPush()
+	{
+		var seq = DOTween.Sequence();
+
+		seq.Append(transform.DOScale(0.75f, 0.1f));
+		seq.Append(transform.DOScale(1.05f, 0.1f));
+		seq.Append(transform.DOScale(1f, 0.1f));
+
+		seq.Play().OnComplete(() => {
+			//시퀀스 작동이 끝났을 때 실행할 것
+		});
+	}
+}
