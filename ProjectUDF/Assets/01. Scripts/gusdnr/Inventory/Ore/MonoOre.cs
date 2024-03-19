83fb9d0d3fd703908df581d599fb0f8bccb8c837
gusdnr1231 <1231phw@gmail.com>
2024-03-19 09:59:31 +0900

    24_03_19
    MonoOre 스크립트 수정 및 각 광물 별 데이터 정리

--- ProjectUDF/Assets/01. Scripts/gusdnr/Inventory/Ore/MonoOre.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Inventory/Ore/MonoOre.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using TMPro;
-using System;
-using Random = UnityEngine.Random;
+using System.Collections;
 
 public class MonoOre : MonoBehaviour
 {
@@ -10,9 +9,8 @@ public class MonoOre : MonoBehaviour
 	public TMP_Text DescText;
 
 	[Header("SO Values")]
-	public OreSO[] OreSOs;
+	public OreSO[] OreDatas;
 	public OreSO CardOreSO;
-
 
 	public void GetOre() => OreInventory.Instance.AddOre(CardOreSO.stats, CardOreSO.value);
 
@@ -22,27 +20,34 @@ public class MonoOre : MonoBehaviour
 		Debug.Log($"Random Ore Setting Complete : {CardOreSO}");
 	}
 
+	private void Start()
+	{
+		StartCoroutine("ResetTest");
+	}
 	#region 외부 호출용 함수들
+
+	private IEnumerator ResetTest()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			ResetOre();
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
 
 	private int tempSO = 0;
 	private void SetRandomOre()
 	{
-		tempSO = Random.Range(0, OreSOs.Length - 1);
-		SetData();
-	}
-
-	public void SetHealOre()
-	{
-		tempSO = OreSOs.Length;
+		tempSO = Random.Range(0, OreDatas.Length);
 		SetData();
 	}
 
 	public void ResetOre()
 	{
-		int resetTempSO = Random.Range(0, OreSOs.Length - 1);
+		int resetTempSO = Random.Range(0, OreDatas.Length);
 		while (resetTempSO == tempSO)
 		{
-			resetTempSO = Random.Range(0, OreSOs.Length - 1);
+			resetTempSO = Random.Range(0, OreDatas.Length);
 		}
 		tempSO = resetTempSO;
 		SetData();
@@ -50,20 +55,10 @@ public class MonoOre : MonoBehaviour
 
 	public void SetData()
 	{
-		CardOreSO = OreSOs[tempSO];
+		CardOreSO = OreDatas[tempSO];
 		#region 설명 문자열 단행 추가
-		string originalDesc = CardOreSO.OreDesc;
-		string searchWord = "광석";
-		int index = originalDesc.IndexOf(searchWord);
-		if (index == -1)
-		{
-			Debug.Log("검색 문자열을 찾을 수 없습니다.");
-			return;
-		}
-		// 검색 문자열 바로 뒤에 줄바꿈 문자열을 삽입하여 새로운 문자열을 생성
-		string desc = 
-			originalDesc.Substring(0, index + searchWord.Length) + 
-			Environment.NewLine	+ originalDesc.Substring(index + searchWord.Length);
+		string desc = CardOreSO.OreDesc;
+		desc = desc.Replace(",", "\n");
 		#endregion
 		NameText.text = CardOreSO.OreName;
 		DescText.text = desc;
