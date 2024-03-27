c1048269616cf848d173f6e4c3d4dbb6dd91332f
gusdnr1231 <1231phw@gmail.com>
2024-03-27 18:04:13 +0900

    24_03_27
    적 FSM 수정 시작

--- ProjectUDF/Assets/01. Scripts/gusdnr/Enemy/FSM/States/Default/EnemyAttackState.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Enemy/FSM/States/Default/EnemyAttackState.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 
 public class EnemyAttackState : EnemyState
 {
-	private List<EnemyAttackPatternBase> attackPatterns = new List<EnemyAttackPatternBase>(); // 다양한 공격 패턴을 담을 리스트
+	private EnemyAttackPatternBase attackPattern;// 공격 패턴
 	private Coroutine attackCoroutine; // 현재 실행 중인 공격 코루틴
 
 	public EnemyAttackState(EnemyMain enemy, EnemyStateMachine enemyStateMachine) : base(enemy, enemyStateMachine)
 	{
-		// 개체 내부에서 EnemyAttackPatternBase를 상속받은 모든 스크립트를 찾아 리스트에 추가
-		EnemyAttackPatternBase[] foundPatterns = enemy.GetComponents<EnemyAttackPatternBase>();
-		foreach (EnemyAttackPatternBase pattern in foundPatterns)
-		{
-			attackPatterns.Add(pattern);
-		}
+		attackPattern = enemy.GetComponent<EnemyAttackPatternBase>();
 	}
 
 	public override void EnterState()
 	{
 		base.EnterState();
 		// 원하는 공격 패턴 선택 및 실행
-		if (attackPatterns.Count > 0)
-		{
-			int randomIndex = Random.Range(0, attackPatterns.Count);
-			attackCoroutine = enemy.StartCoroutine(AttackRoutine(attackPatterns[randomIndex]));
-		}
-		else
-		{
-			Debug.LogWarning("No attack patterns found in the enemy object.");
-			enemyStateMachine.ChangeState(enemy.IdleState);
-		}
+		attackCoroutine = enemy.StartCoroutine(AttackRoutine(attackPattern));
 	}
 
 	public override void ExitState()
