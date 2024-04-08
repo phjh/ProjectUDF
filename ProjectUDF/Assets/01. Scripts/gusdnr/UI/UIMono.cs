6edf59e9ee7ad066b93a2fd9ef213fdd14ba21d0
gusdnr1231 <1231phw@gmail.com>
2024-04-08 18:47:16 +0900

    24_04_08
    반복 호출되어 오류가 발생하는 부분 수정

--- ProjectUDF/Assets/01. Scripts/gusdnr/UI/UIMono.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/UI/UIMono.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public abstract class UIMono : MonoBehaviour
 {
-    private UIManager um = UIManager.Instance;
-
     public abstract void ShowUI();
     public abstract void CloseUI();
 }
