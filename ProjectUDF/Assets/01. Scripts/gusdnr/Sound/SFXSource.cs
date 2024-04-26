03105c309030f31b63f5db78c94dc0b448db9fa3
gusdnr1231 <1231phw@gmail.com>
2024-04-26 15:04:52 +0900

    24_04_26
    Fixing Sound Manager to Pooling

--- ProjectUDF/Assets/01. Scripts/gusdnr/Sound/SFXSource.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Sound/SFXSource.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SFXSource : MonoBehaviour
 {
@@ -8,22 +9,28 @@ public class SFXSource : MonoBehaviour
     public bool IsPlaying {  get; private set; }
     private float clipLength = 0f;
 
+    public AudioSource GetSource()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        return audioPlayer;
+    }
+
     public void InitObject()
     {
         if(audioPlayer == null) audioPlayer = GetComponent<AudioSource>();
         audioPlayer.playOnAwake = false;
         audioPlayer.loop = false;
-
-        if(audioPlayer.isPlaying) audioPlayer.Stop();
+		Debug.Assert(audioPlayer.outputAudioMixerGroup != null, "Audio Mixer Group is Not defined!");
+		if (audioPlayer.isPlaying) audioPlayer.Stop();
         audioPlayer.clip = null;
     }
 
     public void PopAudio(AudioClip sfxClip, Transform point, float volume = 0.5f, float pitch = 1.0f)
     {
-        IsPlaying = true;
-
         gameObject.SetActive(IsPlaying);
         transform.position = point.position;
+
+        IsPlaying = true;
 
         audioPlayer.pitch = pitch;
         audioPlayer.PlayOneShot(sfxClip, volume);
@@ -35,10 +42,14 @@ public class SFXSource : MonoBehaviour
     public void PushAudio()
     {
         IsPlaying = false;
-
+        if(audioPlayer.isPlaying && audioPlayer.clip != null)
+        {
+            audioPlayer.Stop();
+            audioPlayer.clip = null;
+        }
         transform.position = Vector3.zero;
-        clipLength = 0;
         audioPlayer.clip = null;
+        clipLength = 0;
         gameObject.SetActive(IsPlaying);
     }
 }
