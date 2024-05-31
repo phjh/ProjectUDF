ce28eedb829c9d9a4f4409c84367514c1ba51a2f
gusdnr1231 <1231phw@gmail.com>
2024-05-31 15:13:14 +0900

    24_05_30
    Make Bullet's to use boss Pattern

--- ProjectUDF/Assets/01. Scripts/gusdnr/Boss/KingToad/PatternDropStone.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/Boss/KingToad/PatternDropStone.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Pattern_DropStone", menuName = "SO/Boss/KingToad/Pattern_DropStone")]
 public class PatternDropStone : BossPattern
 {
-	[SerializeField] private float AttackRadius = 1.5f;
-
 	private Vector3 TargetPos = Vector3.zero;
 
 	public override void Initialize(BossMain bossMain)
@@ -21,10 +19,12 @@ public class PatternDropStone : BossPattern
 	public override void ActivePattern()
 	{
 		TargetPos = bossMain.TargetTrm.position;
-		CircleCollider2D AttackRange = new CircleCollider2D();
-		AttackRange.radius = AttackRadius;
 		Debug.Log("보스 낙석 장판 POP");
 		Debug.Log("보스 낙석 장판 PUSH");
+		if(IsActive)
+		{
+			ExitPattern();
+		}
 	}
 
 	public override void ExitPattern()
