03f507d64bcf5fc9ef5823a423e019634db4efa4
gusdnr1231 <1231phw@gmail.com>
2024-06-15 23:51:18 +0900

    24_06_15
    Making Result Scene

--- ProjectUDF/Assets/01. Scripts/gusdnr/UI/GameResultLoader.cs
+++ ProjectUDF/Assets/01. Scripts/gusdnr/UI/GameResultLoader.cs
@@ -5,11 +5,60 @@ using UnityEngine;
 
 public class GameResultLoader : MonoBehaviour
 {
+    [SerializeField] private string DieText;
+    [SerializeField] private string TimeOutText;
+    [SerializeField] private string ClearText;
+
     private GameResultData ResultData { get; set;  } = new GameResultData();
 
-    public void GetResultDataInGameManager()
+	private void OnEnable()
+	{
+		InGameSceneManager.OnStartLoadScene += GetResultDataInGameManager;
+	}
+
+	private void OnDisable()
+	{
+		InGameSceneManager.OnStartLoadScene -= GetResultDataInGameManager;
+	}
+
+	public void GetResultDataInGameManager()
     {
         ResultData = new GameResultData();
         ResultData = GameManager.Instance.resultData;
+
+        if(ResultData != null) SetResultUI();
+	}
+
+    private void SetResultUI()
+    {
+        if(ResultData == null)
+        {
+            Debug.LogWarning("Result Data is Null [Error: Null Ref]");
+            return;
+        }
+
+		ConvertResultStateToString();
+
+	}
+
+    private string ConvertResultStateToString()
+    {
+        switch(ResultData.ResultState)
+        {
+            case GameResults.None:
+            case GameResults.Playing:
+                break;
+
+            case GameResults.DiePlayer:
+                return DieText;
+            case GameResults.TimeOut:
+                return TimeOutText;
+            case GameResults.ClearGame:
+                return ClearText;
+            
+            default: return null;
+		}
+
+        return null;
     }
 }
