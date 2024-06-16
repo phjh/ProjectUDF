6e4d5f1d81c93149d1b90ff2c9613fea7735f4fe
gusdnr1231 <1231phw@gmail.com>
2024-06-17 00:41:15 +0900

    24_06_17
    Edit Script

--- ProjectUDF/Assets/01. Scripts/phjh/UI/UIToolkit/StartScene.cs
+++ ProjectUDF/Assets/01. Scripts/phjh/UI/UIToolkit/StartScene.cs
@@ -7,32 +7,7 @@ using UnityEngine.UIElements;
 
 public class StartScene : MonoBehaviour
 {
-    private UIDocument _uiDocument;
     public SceneList NextScene;
-
-    private void Awake()
-    {
-        _uiDocument = GetComponent<UIDocument>();
-    }
-
-    private void OnEnable()
-    {
-        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByBuildIndex(0))
-            return;
-
-        VisualElement root = _uiDocument.rootVisualElement;
-        //이녀석은 UI Object다 . 모든 애들의 근간이 된다.
-
-        Button btn = root.Q<Button>("StartBtn");
-        //Query => 질의, 질문
-
-        btn.RegisterCallback<ClickEvent>(e =>
-        {
-            SceneManager.LoadScene((int)NextScene);
-        });
-
-
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
